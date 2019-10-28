c3df05374924deef2940a5965905ef9119940872
Fedorosh <Vapaiss1@gmail.com>
2019-10-28 14:06:45 +0100

    not working wall script

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     private Rigidbody2D bumper;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +15,23 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("left"))
+
+        if (Input.GetKey("left"))
         {
-            bumper.AddForce(new Vector2(-10.0f, 0.0f));
-            
+            bumper.AddForce(new Vector2(-speed, 0.0f));
         }
         else if (Input.GetKey("right"))
         {
-            bumper.AddForce(new Vector2(10.0f, 0.0f));
+            bumper.AddForce(new Vector2(speed, 0.0f));
         }
         else bumper.velocity = Vector2.zero;
+
+        if (bumper.position.x >= 10.4f && bumper.position.x <= -10.4f)
+        {
+            if (bumper.position.x < 0)
+                bumper.position = new Vector2(-10.4f, -5.5f);
+            if (bumper.position.x > 0)
+                bumper.position = new Vector2(10.4f, -5.5f);
+        }
     }
 }
