de22d465eb89776a89082ed6880e85a1a1ff3190
Fedorosh <Vapaiss1@gmail.com>
2019-10-28 12:54:23 +0100

    Initial commit

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Rigidbody2D bumper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bumper = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left"))
+        {
+            bumper.AddForce(new Vector2(-10.0f, 0.0f));
+            
+        }
+        else if (Input.GetKey("right"))
+        {
+            bumper.AddForce(new Vector2(10.0f, 0.0f));
+        }
+        else bumper.velocity = Vector2.zero;
+    }
+}
