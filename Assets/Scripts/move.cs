7d55684b527b30c854c9a12a1f76ccf3af47c7bb
Fedorosh <Vapaiss1@gmail.com>
2019-10-31 10:19:25 +0100

    Beautified Code

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -8,30 +8,44 @@ public class move : MonoBehaviour
     public float speed;
     private float actual_x;
     private float y;
+    private enum sign : int { plus = 1 ,minus = -1 };
     // Start is called before the first frame update
     void Start()
     {
         bumper = GetComponent<Rigidbody2D>();
-        actual_x = bumper.transform.position.x;
+        ActualPosition();
         y = bumper.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("right") && !Input.GetKey("left"))
-        {
-            //bumper.AddForce(new Vector2(-speed, 0.0f));
-            bumper.transform.position = new Vector2(actual_x + speed, y);
-        }
-        if (Input.GetKey("left") && !Input.GetKey("right"))
-        {
-            //bumper.AddForce(new Vector2(speed, 0.0f));
-            bumper.transform.position = new Vector2(actual_x - speed, y);
-        }
+            if (Input.GetKey("right") && !Input.GetKey("left"))
+            {
+                //bumper.AddForce(new Vector2(-speed, 0.0f));
+                MoveBumper((float)sign.plus);
+            }
+            if (Input.GetKey("left") && !Input.GetKey("right"))
+            {
+                //bumper.AddForce(new Vector2(speed, 0.0f));
+                MoveBumper((float)sign.minus);
+            }
+
         //else bumper.velocity = Vector2.zero;
+        //Vector2 temp = transform.position;
+        //temp.x += Input.GetKeyDown(KeyCode.RightArrow);
+        //bumper.transform.position = new Vector2(actual_x + speed, y);
 
+        ActualPosition();
+
+    }
+
+    private void MoveBumper(float sign)
+    {
+        bumper.transform.position = new Vector2(actual_x  + (speed * sign), y);
+    }
+    private void ActualPosition()
+    {
         actual_x = bumper.transform.position.x;
-
     }
 }
