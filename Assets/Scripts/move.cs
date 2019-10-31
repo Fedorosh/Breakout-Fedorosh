958a9e993b30461195fdc0abf3e49d5bc6e0665d
Fedorosh <Vapaiss1@gmail.com>
2019-10-31 08:51:45 +0100

    Create Tile and improve move script

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -6,23 +6,32 @@ public class move : MonoBehaviour
 {
     private Rigidbody2D bumper;
     public float speed;
+    private float actual_x;
+    private float y;
     // Start is called before the first frame update
     void Start()
     {
         bumper = GetComponent<Rigidbody2D>();
+        actual_x = bumper.transform.position.x;
+        y = bumper.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("left"))
+        if (Input.GetKey("right") && !Input.GetKey("left"))
         {
-            bumper.AddForce(new Vector2(-speed, 0.0f));
+            //bumper.AddForce(new Vector2(-speed, 0.0f));
+            bumper.transform.position = new Vector2(actual_x + speed, y);
         }
-        else if (Input.GetKey("right"))
+        if (Input.GetKey("left") && !Input.GetKey("right"))
         {
-            bumper.AddForce(new Vector2(speed, 0.0f));
+            //bumper.AddForce(new Vector2(speed, 0.0f));
+            bumper.transform.position = new Vector2(actual_x - speed, y);
         }
-        else bumper.velocity = Vector2.zero;
+        //else bumper.velocity = Vector2.zero;
+
+        actual_x = bumper.transform.position.x;
+
     }
 }
