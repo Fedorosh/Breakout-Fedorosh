87427183e3c60e925967ee203e5273a9f5658df4
Fedorosh <Vapaiss1@gmail.com>
2019-11-14 09:34:43 +0100

    mouse collision

--- Assets/Scripts/MoveBumpers.cs
+++ Assets/Scripts/MoveBumpers.cs
@@ -8,32 +8,29 @@ public class MoveBumpers : Move
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("right") && !Input.GetKey("left") && !isCollidedRight)
-            MoveBumper((float)sign.plus);
-        if (Input.GetKey("left") && !Input.GetKey("right") && !isCollidedLeft)
-            MoveBumper((float)sign.minus);
+        if (Input.GetKey("right") && !Input.GetKey("left") && !isCollidedRight) MoveBumper((float)sign.plus);
+
+        if (Input.GetKey("left") && !Input.GetKey("right") && !isCollidedLeft) MoveBumper((float)sign.minus);
 
         float translation = Input.GetAxis("Mouse X");
 
-        bumper.transform.Translate(translation, 0, 0);
-
+        if ((isCollidedLeft && translation < 0) || (isCollidedRight && translation > 0))
+            bumper.transform.Translate(0, 0, 0);
+        else
+            bumper.transform.Translate(translation, 0, 0);
         ActualPosition();
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "wallleft")
-            isCollidedLeft = true;
-        if (collision.collider.tag == "wallright")
-            isCollidedRight = true;
+        if (collision.collider.tag == "wallleft") isCollidedLeft = true;
+        if (collision.collider.tag == "wallright") isCollidedRight = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.tag == "wallright" )
-            isCollidedRight = false;
-        if (collision.collider.tag == "wallleft")
-            isCollidedLeft = false;
+        if (collision.collider.tag == "wallright" ) isCollidedRight = false;
+        if (collision.collider.tag == "wallleft") isCollidedLeft = false;
     }
 }
